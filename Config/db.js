23f0aa0814f221e34d697220d32a3a303ff6384a
Umer Saleem <162819866+UmerSaleemUltra@users.noreply.github.com>
2025-06-21 22:44:00 +0500

    update

--- Config/db.js
+++ Config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 
-mongoose.connect("mongodb+srv://Incorz:2668@cluster0.ewwkzzw.mongodb.net/incorz?retryWrites=true&w=majority&appName=Cluster0/incorz") 
+mongoose.connect('mongodb+srv://umer:171175@cluster0.zbqcm.mongodb.net/tic-tac-toe')
  
 export default mongoose
