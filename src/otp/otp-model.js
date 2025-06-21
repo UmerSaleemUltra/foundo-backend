5f9b35b014c29200c37720d4c5149464a3b72115
Umer Saleem <162819866+UmerSaleemUltra@users.noreply.github.com>
2025-06-22 00:36:23 +0500

    update

--- src/otp/otp-model.js
+++ src/otp/otp-model.js
@@ -1,33 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const otpSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        trim: true,
-        default: "",
-    },
-    otp: {
-        type: String,
-        trim: true,
-        default: "",
-    },
-    otp_expiry: {
-        type: Date,
-        default: Date.now
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    },
+  email: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  otp_expiry: {
+    type: Date,
+    required: true,
+  }
 });
 
-otpSchema.pre('save', function (next) {
-    this.updated_at = Date.now();
-    next();
-});
-
-export default otpSchema
+export default mongoose.model('Otp', otpSchema);
